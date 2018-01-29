621ce982d416362b6d45714312481209e14bcc44
RafaelSouzaValle <rafaelsouzavalle@gmail.com>
2018-01-29 00:53:41 -0200

    Crie a Classe Main
    /**
    * Classe principal
    *
    * @author Rafael.Valle
    *
    */

---
+++ src/jogodaforca/jogo/Main.java
@@ -0,0 +1,22 @@
+package jogodaforca.jogo;
+
+/**
+ * Classe principal
+ * 
+ * @author Rafael.Valle
+ *
+ */
+public class Main {
+
+	/**
+	 * Método principal : que inicializa o jogo
+	 * 
+	 * @author Rafael.Valle
+	 *
+	 */
+	public static void main(String[] args) throws Exception {
+		
+		Jogo jogo = new Jogo();
+		jogo.iniciar();
+	}
+}
